6fac9dfdda1c3ffd0c9dc2df2eebd80f7cc226d6
jawkills <67185214+jawkills@users.noreply.github.com>
2020-07-12 01:31:34 +0700

    Update gas15.php

--- gas15.php
+++ gas15.php
@@ -2,8 +2,8 @@
 date_default_timezone_set('Asia/Jakarta');
 include "function15.php";
 echo color("nevy"," ===================================\n");
-echo color("yellow","Klaim Voucher 15+10+5\n");
-echo color("green","Jawkills & CUAN FOR MY LIFE\n");
+echo color("yellow"," Klaim Voucher 15+10+5\n");
+echo color("green"," Jawkills & CUAN FOR MY LIFE\n");
 echo color("blue"," Time    : ".date('[d-m-Y] [H:i:s]')."	\n");
 echo color("nevy"," ===================================\n");
 echo color("white","Token: ");
